61e3a9e7ceb2a9c7f09f87d5609aa8fe164c321f
SandaruWithanage <160226615+SandaruWithanage@users.noreply.github.com>
2025-07-15 00:43:45 +0530

    Update BookService for find book list on borrow management

--- src/main/java/org/example/service/BookService.java
+++ src/main/java/org/example/service/BookService.java
@@ -5,14 +5,17 @@ import org.example.dto.BookDTO;
 import java.sql.SQLException;
 import java.util.List;
 
-/**
- * The service layer interface for Book-related business logic.
- * The generateNextBookId method has been removed.
- */
 public interface BookService {
     boolean addBook(BookDTO bookDTO) throws SQLException;
     boolean updateBook(BookDTO bookDTO) throws SQLException;
     boolean deleteBook(String bookId) throws SQLException;
     BookDTO getBookById(String bookId) throws SQLException;
     List<BookDTO> getAllBooks() throws SQLException;
+
+    /**
+     * Gets a list of all books that are currently available.
+     * @return A list of available BookDTOs.
+     * @throws SQLException if a database error occurs.
+     */
+    List<BookDTO> getAvailableBooks() throws SQLException;
 }
