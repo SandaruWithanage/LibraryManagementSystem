0cc00a6469dddc90fed7fdd7c0624ead01be7193
SandaruWithanage <160226615+SandaruWithanage@users.noreply.github.com>
2025-07-15 12:06:17 +0530

    Update Book section for auto ID generation

--- src/main/java/org/example/service/BookService.java
+++ src/main/java/org/example/service/BookService.java
@@ -5,12 +5,17 @@ import org.example.dto.BookDTO;
 import java.sql.SQLException;
 import java.util.List;
 
+/**
+ * The service layer interface for Book-related business logic.
+ * This version is now complete and includes all necessary methods.
+ */
 public interface BookService {
     boolean addBook(BookDTO bookDTO) throws SQLException;
     boolean updateBook(BookDTO bookDTO) throws SQLException;
     boolean deleteBook(String bookId) throws SQLException;
     BookDTO getBookById(String bookId) throws SQLException;
     List<BookDTO> getAllBooks() throws SQLException;
+    String generateNextBookId() throws SQLException;
 
     /**
      * Gets a list of all books that are currently available.
